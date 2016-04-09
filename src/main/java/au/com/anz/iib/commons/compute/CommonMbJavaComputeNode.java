237ebc0ada4e3f3b13f322cdd1f65fad04d96189
edwinquaihoi <edwinquaihoi@gmail.com>
2016-04-09 11:34:13 +1000

    Initial Commit

---
+++ src/main/java/au/com/anz/iib/commons/compute/CommonMbJavaComputeNode.java
@@ -0,0 +1,43 @@
+package au.com.anz.iib.commons.compute;
+import au.com.anz.utils.TerminalAssemblyPair;
+
+import com.ibm.broker.javacompute.MbJavaComputeNode;
+import com.ibm.broker.plugin.MbException;
+import com.ibm.broker.plugin.MbMessageAssembly;
+import com.ibm.broker.plugin.MbOutputTerminal;
+import com.ibm.broker.plugin.MbUserException;
+
+
+public abstract class CommonMbJavaComputeNode extends MbJavaComputeNode {
+	
+	public abstract TerminalAssemblyPair execute(MbMessageAssembly inAssembly) throws Exception;
+
+	public MbOutputTerminal getOutTerminal() {
+		return getOutputTerminal("out");
+	}
+	
+	public MbOutputTerminal getAltTerminal() {
+		return getOutputTerminal("alt");
+	}
+
+	public void evaluate(MbMessageAssembly inAssembly) throws MbException {
+
+		try {			
+			TerminalAssemblyPair pair = execute(inAssembly);
+			pair.getKey().propagate(pair.getValue());
+
+		} catch (MbException e) {
+			// Re-throw to allow Broker handling of MbException
+			throw e;
+		} catch (RuntimeException e) {
+			// Re-throw to allow Broker handling of RuntimeException
+			throw e;
+		} catch (Exception e) {
+			// Consider replacing Exception with type(s) thrown by user code
+			// Example handling ensures all exceptions are re-thrown to be handled in the flow
+			throw new MbUserException(this, "evaluate()", "", "", e.toString(),
+					null);
+		}
+	}
+
+}
---
+++ src/main/java/au/com/anz/utils/TerminalAssemblyPair.java
@@ -0,0 +1,31 @@
+package au.com.anz.utils;
+
+import com.ibm.broker.plugin.MbMessageAssembly;
+import com.ibm.broker.plugin.MbOutputTerminal;
+
+public class TerminalAssemblyPair extends Pair<MbOutputTerminal, MbMessageAssembly> {
+
+	public TerminalAssemblyPair() {
+		super();
+	}
+	
+	public TerminalAssemblyPair(MbOutputTerminal outputTerminal,MbMessageAssembly messageAssembly) {
+		super(outputTerminal, messageAssembly);
+	}
+	
+	public MbOutputTerminal getOutputTerminal() {
+		return getKey();		
+	}
+	
+	public void setOutputTerminal(MbOutputTerminal mbOutputTerminal) {
+		setKey(mbOutputTerminal);
+	}
+
+	MbMessageAssembly getMessageAssembly() {
+		return getValue();		
+	}
+	
+	public void setMessageAssembly(MbMessageAssembly mbMessageAssembly) {
+		setValue(mbMessageAssembly);
+	}
+}
